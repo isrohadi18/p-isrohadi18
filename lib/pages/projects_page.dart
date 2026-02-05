eee10ace09c75d299a967456cda3c19d5b583091
RohadiDJ <rohadii861@gmail.com>
2026-02-05 18:06:39 +0700

    modif project

--- lib/pages/projects_page.dart
+++ lib/pages/projects_page.dart
@@ -11,7 +11,12 @@ class ProjectsPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final width = MediaQuery.of(context).size.width;
-    final crossAxisCount = width < 600 ? 1 : width < 900 ? 2 : 3;
+    final crossAxisCount =
+        width < 600
+            ? 1
+            : width < 900
+            ? 2
+            : 3;
 
     return Container(
       height: MediaQuery.of(context).size.height,
@@ -26,9 +31,7 @@ class ProjectsPage extends StatelessWidget {
               fontWeight: FontWeight.bold,
               fontSize: width < 600 ? 24 : 32,
             ),
-          ).animate()
-            .fadeIn(duration: 600.ms)
-            .slideX(begin: -0.2),
+          ).animate().fadeIn(duration: 600.ms).slideX(begin: -0.2),
           SizedBox(height: width < 600 ? 20 : 30),
           Expanded(
             child: GridView.count(
@@ -37,37 +40,53 @@ class ProjectsPage extends StatelessWidget {
               crossAxisSpacing: width < 600 ? 16 : 20,
               mainAxisSpacing: width < 600 ? 16 : 20,
               childAspectRatio: width < 600 ? 0.8 : 1.0, // Adjusted ratio
-              children: [
-                ProjectCard(
-                  title: 'Sistem Pengajuan PKN (Praktek Kerja Nyata)',
-                  description: 'Sistem pengajuan PKN (Praktek Kerja Nyata) menggunakan koneksi Database mySql sebagai tempat penyimpanan data user ataupun inputan user',
-                  technologies: ['Java', 'Mysql'],
-                  imageUrl: 'assets/images/proyek1.png',
-                  projectUrl: 'https://github.com/Rysalb/TubesPbo',
-                  isAssetImage: true,
-                ),
-                ProjectCard(
-                  title: 'Ladangku.id',
-                  description: 'Ladangku.id is a web-based application that provides a platform for farmers to sell their products online.',
-                  technologies: ['HTML', 'CSS'],
-                  imageUrl: 'assets/images/proyek2.png',
-                  projectUrl: 'https://github.com/Rysalb/Ladangku-UI',
-                  isAssetImage: true,
-                ),
-                ProjectCard(
-                  title: 'Aplikasi Sistem Manajemen Kos',
-                  description: 'Aplikasi manajemen kos adalah solusi digital untuk mengelola operasional kos secara efisien. Aplikasi ini terdiri dari dua platform: backend menggunakan Laravel dan frontend menggunakan Flutter.',
-                  technologies: ['Flutter', 'Laravel', 'MySQL'],
-                  imageUrl: 'assets/images/proyek3.png',
-                  projectUrl: 'https://github.com/Rysalb/fe_kos_hansarin',
-                  isAssetImage: true,
-                ),
-              ].mapIndexed((index, card) => card.animate(
-                delay: (200 * index).ms)
-                .fadeIn()
-                .slideY(begin: 0.2)
-                .scale(begin: const Offset(0.8, 0.8))
-              ).toList(),
+              children:
+                  [
+                        ProjectCard(
+                          title: 'Sistem Pengajuan PKN (Praktek Kerja Nyata)',
+                          description:
+                              'Sistem pengajuan PKN (Praktek Kerja Nyata) menggunakan koneksi Database mySql sebagai tempat penyimpanan data user ataupun inputan user',
+                          technologies: ['Java', 'Mysql'],
+                          imageUrl: 'assets/images/proyek1.png',
+                          githubUrl: 'https://github.com/Rysalb/TubesPbo',
+                          demoUrl:
+                              'https://isrohadi18.github.io/CodingCamp-RohadiDJ/',
+                          downloadUrl: '',
+                          demoType: DemoType.video,
+                          isAssetImage: true,
+                        ),
+                        ProjectCard(
+                          title: 'Ladangku.id',
+                          description:
+                              'Ladangku.id is a web-based application that provides a platform for farmers to sell their products online.',
+                          technologies: ['HTML', 'CSS'],
+                          imageUrl: 'assets/images/proyek2.png',
+                          githubUrl: 'https://github.com/Rysalb/Ladangku-UI',
+                          demoUrl:
+                              'https://isrohadi18.github.io/CodingCamp-RohadiDJ/',
+                          downloadUrl: '',
+                          demoType: DemoType.web,
+                          isAssetImage: true,
+                        ),
+                        ProjectCard(
+                          title: 'Aplikasi Sistem Manajemen Kos',
+                          description:
+                              'Aplikasi manajemen kos adalah solusi digital untuk mengelola operasional kos secara efisien. Aplikasi ini terdiri dari dua platform: backend menggunakan Laravel dan frontend menggunakan Flutter.',
+                          technologies: ['Flutter', 'Laravel', 'MySQL'],
+                          imageUrl: 'assets/images/proyek3.png',
+                          githubUrl:
+                              'https://github.com/Rysalb/fe_kos_hansarin',
+                          isAssetImage: true,
+                        ),
+                      ]
+                      .mapIndexed(
+                        (index, card) => card
+                            .animate(delay: (200 * index).ms)
+                            .fadeIn()
+                            .slideY(begin: 0.2)
+                            .scale(begin: const Offset(0.8, 0.8)),
+                      )
+                      .toList(),
             ),
           ),
         ],
@@ -76,12 +95,17 @@ class ProjectsPage extends StatelessWidget {
   }
 }
 
+enum DemoType { none, video, web }
+
 class ProjectCard extends StatelessWidget {
   final String title;
   final String description;
   final List<String> technologies;
   final String imageUrl;
-  final String projectUrl;
+  final String githubUrl;
+  final String? demoUrl;
+  final String? downloadUrl;
+  final DemoType demoType;
   final bool isAssetImage;
 
   const ProjectCard({
@@ -90,14 +114,17 @@ class ProjectCard extends StatelessWidget {
     required this.description,
     required this.technologies,
     required this.imageUrl,
-    required this.projectUrl,
+    required this.githubUrl,
+    this.demoUrl,
+    this.downloadUrl,
+    this.demoType = DemoType.none,
     this.isAssetImage = false,
   });
 
-  Future<void> _launchUrl() async {
-    final Uri url = Uri.parse(projectUrl);
-    if (!await launchUrl(url)) {
-      throw Exception('Could not launch $url');
+  Future<void> _launch(String url) async {
+    final Uri uri = Uri.parse(url);
+    if (!await launchUrl(uri, mode: LaunchMode.externalApplication)) {
+      throw Exception('Could not launch $uri');
     }
   }
 
@@ -105,15 +132,13 @@ class ProjectCard extends StatelessWidget {
   Widget build(BuildContext context) {
     final width = MediaQuery.of(context).size.width;
     final isMobile = width < 600;
-    
+
     return Card(
       elevation: 4,
       color: Theme.of(context).cardColor,
-      shape: RoundedRectangleBorder(
-        borderRadius: BorderRadius.circular(12),
-      ),
+      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(12)),
       child: InkWell(
-        onTap: _launchUrl,
+        onTap: null,
         borderRadius: BorderRadius.circular(12),
         child: Column(
           children: [
@@ -122,11 +147,14 @@ class ProjectCard extends StatelessWidget {
               aspectRatio: 16 / 9,
               child: Container(
                 decoration: BoxDecoration(
-                  borderRadius: const BorderRadius.vertical(top: Radius.circular(12)),
+                  borderRadius: const BorderRadius.vertical(
+                    top: Radius.circular(12),
+                  ),
                   image: DecorationImage(
-                    image: isAssetImage 
-                        ? AssetImage(imageUrl) as ImageProvider
-                        : NetworkImage(imageUrl),
+                    image:
+                        isAssetImage
+                            ? AssetImage(imageUrl) as ImageProvider
+                            : NetworkImage(imageUrl),
                     fit: BoxFit.cover,
                   ),
                 ),
@@ -149,48 +177,62 @@ class ProjectCard extends StatelessWidget {
                       maxLines: 2,
                       overflow: TextOverflow.ellipsis,
                     ),
-                    const SizedBox(height: 4),
+                    const SizedBox(height: 8),
                     // Description
-                    Expanded(
+                    SizedBox(
+                      height: isMobile ? 14 : 32, // ± 3–5 baris
                       child: Text(
                         description,
                         style: TextStyle(
                           fontSize: isMobile ? 10 : 12,
                           color: Colors.grey[600],
+                          height: 1.4, // line height biar rapi
                         ),
-                        maxLines: isMobile ? 3 : 4,
+                        maxLines: isMobile ? 3 : 5,
                         overflow: TextOverflow.ellipsis,
                       ),
                     ),
-                    const SizedBox(height: 4),
-                    // Technologies
-                    SizedBox(
-                      height: 24,
-                      child: ListView(
-                        scrollDirection: Axis.horizontal,
-                        physics: const BouncingScrollPhysics(),
-                        children: technologies.map((tech) => Padding(
-                          padding: const EdgeInsets.only(right: 4),
-                          child: Container(
-                            padding: const EdgeInsets.symmetric(
-                              horizontal: 6,
-                              vertical: 2,
-                            ),
-                            decoration: BoxDecoration(
-                              color: Theme.of(context).colorScheme.primary.withOpacity(0.1),
-                              borderRadius: BorderRadius.circular(12),
-                            ),
-                            child: Text(
-                              tech,
-                              style: TextStyle(
-                                fontSize: isMobile ? 9 : 10,
-                                color: Theme.of(context).colorScheme.primary,
-                                fontWeight: FontWeight.bold,
-                              ),
-                            ),
+                    const SizedBox(height: 15),
+                    Wrap(
+                      spacing: 6,
+                      runSpacing: 6,
+                      children:
+                          technologies
+                              .map((tech) => TechBadge(label: tech))
+                              .toList(),
+                    ),
+                    const SizedBox(height: 8),
+
+                    Wrap(
+                      spacing: 8,
+                      runSpacing: 8,
+                      children: [
+                        ProjectActionButton(
+                          label: 'GitHub',
+                          icon: Icons.code,
+                          onPressed: () => _launch(githubUrl),
+                        ),
+
+                        if (demoUrl != null && demoType != DemoType.none)
+                          ProjectActionButton(
+                            label:
+                                demoType == DemoType.video
+                                    ? 'Demo Video'
+                                    : 'Demo Web',
+                            icon:
+                                demoType == DemoType.video
+                                    ? Icons.play_circle_outline
+                                    : Icons.open_in_new,
+                            onPressed: () => _launch(demoUrl!),
                           ),
-                        )).toList(),
-                      ),
+
+                        if (downloadUrl != null)
+                          ProjectActionButton(
+                            label: 'Download',
+                            icon: Icons.download,
+                            onPressed: () => _launch(downloadUrl!),
+                          ),
+                      ],
                     ),
                   ],
                 ),
@@ -198,6 +240,78 @@ class ProjectCard extends StatelessWidget {
             ),
           ],
         ),
+      ),
+    );
+  }
+}
+
+class TechBadge extends StatelessWidget {
+  final String label;
+
+  const TechBadge({super.key, required this.label});
+
+  @override
+  Widget build(BuildContext context) {
+    final isDark = Theme.of(context).brightness == Brightness.dark;
+
+    return Container(
+      padding: const EdgeInsets.symmetric(horizontal: 10, vertical: 4),
+      decoration: BoxDecoration(
+        color:
+            isDark
+                ? Theme.of(context).colorScheme.primary.withOpacity(0.2)
+                : Theme.of(context).colorScheme.primary.withOpacity(0.1),
+        borderRadius: BorderRadius.circular(20),
+        border: Border.all(
+          color: Theme.of(context).colorScheme.primary.withOpacity(0.3),
+        ),
+      ),
+      child: Text(
+        label,
+        style: TextStyle(
+          fontSize: 10,
+          fontWeight: FontWeight.w600,
+          color: Theme.of(context).colorScheme.primary,
+        ),
+      ),
+    );
+  }
+}
+
+class ProjectActionButton extends StatelessWidget {
+  final String label;
+  final IconData icon;
+  final VoidCallback onPressed;
+
+  const ProjectActionButton({
+    super.key,
+    required this.label,
+    required this.icon,
+    required this.onPressed,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    final colorScheme = Theme.of(context).colorScheme;
+    final isDark = Theme.of(context).brightness == Brightness.dark;
+
+    return OutlinedButton.icon(
+      onPressed: onPressed,
+      icon: Icon(icon, size: 16, color: colorScheme.primary),
+      label: Text(
+        label,
+        style: TextStyle(
+          fontSize: 12,
+          fontWeight: FontWeight.w600,
+          color: colorScheme.primary,
+        ),
+      ),
+      style: OutlinedButton.styleFrom(
+        padding: const EdgeInsets.symmetric(horizontal: 14, vertical: 8),
+        side: BorderSide(
+          color: colorScheme.primary.withOpacity(isDark ? 0.6 : 0.8),
+        ),
+        shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(20)),
       ),
     );
   }
